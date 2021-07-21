05a20dddd2091c0bbc3f6ec2ab07ee79b3030a05
João Baiochi <jao.baiochi@gmail.com>
2021-07-21 20:38:02 -0300

    removed comments for cleanner code

--- Modulo_1/Projeto/main.py
+++ Modulo_1/Projeto/main.py
@@ -7,25 +7,15 @@ wordList = readList("Modulo_1\Projeto\medium_list.txt")
 print("Bem vindo ao Jogo da Forca.\n")
 
 while continuar:
-    
-    # escolhendo as opcoes do menu
-    menuOpc = int(input(">>> MENU <<<\nEscolha uma opcao:\n1 - Iniciar Jogo\n2 - Mudar dificuldade\n3 - Sair\n"))
 
-    # iniciando o jogo
+    menuOpc = int(input(">>> MENU <<<\nEscolha uma opcao:\n1 - Iniciar Jogo\n2 - Mudar dificuldade\n3 - Sair\n"))
     if menuOpc == 1:
         playGame(wordList)
-
     elif menuOpc == 2:
-        # clear_output(wait = True)
-        wordList = changeLevel()
-        
-    # saindo do programa
+        wordList = changeLevel() 
     elif menuOpc == 3:
-        # clear_output(wait = True)
         print("\nSaindo... Obrigado por jogar!")
         continuar = False
-        
-    # verificacao inválido
     else:
         print("Opcao inválida! Digite novamente: ")
         
