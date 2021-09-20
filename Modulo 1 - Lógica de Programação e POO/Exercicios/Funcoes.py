7127e79f0e5443ace4b0ada9711dfbc7e7dd3e3f
Joao Baiochi <jao.baiochi@gmail.com>
2021-09-19 22:58:11 -0300

    minor bugs solved

--- Modulo 1 - Lógica de Programação e POO/Exercicios/Funcoes.py
+++ Modulo 1 - Lógica de Programação e POO/Exercicios/Funcoes.py
@@ -43,11 +43,12 @@ class Player():
 
 def createDeck():
 
-    return ['A',*range(0,11),'J','Q','K'] * 2
+    return ['A',*range(1,11),'J','Q','K'] * 2
 
 def pickCard(deck):
     # random select a card
     card = random.sample(deck,1)[0]
+    print(f'Card picked: {card}')
     # remove that card from deck
     deck.remove(card)
     return card
@@ -59,6 +60,7 @@ def verifyScore(players):
     _continue = False
 
     # display current score
+    print('\n\u001b[36mRound summary:\u001b[37m')
     for player in players:
         if player.getState() == 'in':
             print(f'Player: {player.getName()}, score: {player.getScore()}')
@@ -68,11 +70,12 @@ def verifyScore(players):
             # store score and name in a tuple
             validPlayers.append((player.getScore(),player.getName()))
         elif player.getState() == 'blackjack':
-            print(f'Player: {player.getName()}, hit a blackjack!')
+            print(f'Player: {player.getName()}, blackjack!')
             validPlayers.append((player.getScore(),player.getName()))
             blackjack += 1
         else:
-            print(f'Player: {player.getName()}, is out!') 
+            print(f'Player: {player.getName()}, is out!')
+    print()
 
     # skip verification and continue the game
     if _continue:
@@ -80,34 +83,38 @@ def verifyScore(players):
 
     # verify two or more winners
     if blackjack > 1:
-        print('Draw!')
+        print('\u001b[32mDraw game!\u001b[37m')
     # display the winner
-    elif blackjack <= 1:
-        print(f'The winner is {max(validPlayers)[1]}, with {max(validPlayers)[0]} points!')
-        return False
+    elif blackjack == 1 or validPlayers is not None:
+        print(f'\u001b[32mThe winner is {max(validPlayers)[1]}, with {max(validPlayers)[0]} points!\u001b[37m')
+    else:
+        print('\u001b[32mNo winners in this game.\u001b[37m')
+    
+    return False
     
 
 def playRound(player, deck):
 
     print(f'Player {player.getName()} turn.')
-    choice = input(f'Current score: {player.getScore()}, buy card? (Y/N)')
+    choice = input(f'\u001b[36mCurrent score: {player.getScore()}, buy card? (Y/N)\u001b[37m')
     
     if choice.upper() == 'Y':
         card = pickCard(deck)
-        deck.remove(card)
         player.updateScore(cardValues[card])
         if player.getScore() > 21:
             print(f'Player {player.getName()} is out! Score: {player.getScore()}')
             player.setState('lose')
         elif player.getScore() == 21:
-            print(f'Player {player.getName()} hit a Black Jack!')
+            print(f'\u001b[31mPlayer {player.getName()} hit a Black Jack!\u001b[37m')
             player.setState('blackjack')
     else:
         player.setState('out')
 
 def initGame():
 
-    playersNumber = int(input('How many players?'))
+    playersNumber = int(input('How many players? (Max 4)'))
+    while playersNumber not in [1,2,3,4]:
+        playersNumber = int(input('Invalid number, please type again:\nHow many players? (Max 4)'))
     players = []
     for index in range(playersNumber):
         players.append(Player( name = input(f'Type the name of the player {index+1}: ')))
@@ -119,7 +126,6 @@ def initGame():
     while _continue:
 
         for player in players:
-            print(player.getState())
             if player.getState() == 'in':
                 playRound(player, deck)
 
@@ -130,7 +136,7 @@ def main():
 
     while not end:
 
-        choice = input('Play a game? (Y/S)')
+        choice = input('Play a game? (Y/S) ')
         if choice.upper() == 'Y':
             initGame()
         else:
